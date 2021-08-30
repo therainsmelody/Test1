fa5873217f05d2f1c98293957a2297de1f4d10df
Cj Johnston <64555196+therainsmelody@users.noreply.github.com>
2021-08-30 19:35:56 +0000

    updated line 6

--- main.cs
+++ main.cs
@@ -3,6 +3,6 @@ using System;
 class Program {
   public static void Main (string[] args) {
     Console.WriteLine ("Hello World");
-    Console.WriteLine ("Hello World 2nd Line");
+    Console.WriteLine ("Hello Hi!");
   }
 }
